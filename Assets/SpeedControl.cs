6be47fe985ce3b1ad038ed864d3931b49c894141
DESKTOP-GM8NRCF\sunlight <sunlight1994@imrlab.hoseo.edu>
2018-01-08 14:36:43 +0900

    no message

--- Assets/SpeedControl.cs
+++ Assets/SpeedControl.cs
@@ -11,26 +11,18 @@ public class SpeedControl : MonoBehaviour {
     private Slider slider;
     public Animator _animator;
     public PlayableDirector _playDi;
-    public Transform _characterTr;
-
-    private Vector3 _currentLocation;
-    private Vector3 _prevLocation;
-    private float _velocity;
-    int i;
+    public Rigidbody _characterRbody;
 	// Use this for initialization
 	void Start () {
         slider = GetComponent<Slider>();
         _playDi.Stop();
         _playDi.Play();
-        _velocity = 0.0f;
         _animator.SetFloat("Speed", 0.0f);
-        _currentLocation = _characterTr.position;
-        _prevLocation = _characterTr.position;
     }
 
     // Update is called once per frame
     void Update () {
-        UpdateVelocity();
+        //_currentLocation = _characterTr.position;
         SetSpeed();
         _playDi.time += speed * Time.deltaTime;
     }
@@ -38,25 +30,10 @@ public class SpeedControl : MonoBehaviour {
     void SetSpeed()
     {
         speed = slider.value * 0.4f;
-        _animator.SetFloat("Speed", _velocity * 7500.0f);
+        _animator.SetFloat("Speed", _characterRbody.velocity.magnitude * 400.0f);
     }
 
     void UpdateVelocity()
     {
-        _currentLocation = _characterTr.position;
-        Vector3 _transLocation = _currentLocation - _prevLocation;
-        if (speed != 0 && _currentLocation == _prevLocation)
-        {
-        }
-        else if (speed == 0 && _currentLocation == _prevLocation)
-        {
-
-        }
-        else
-        {
-            _velocity = _transLocation.magnitude;
-        }
-        _prevLocation = _characterTr.position;
-
     }
 }
