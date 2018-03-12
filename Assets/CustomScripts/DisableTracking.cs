9e96947c1412b069a8e54839192733fd96243a12
DESKTOP-GM8NRCF\sunlight <sunlight1994@imrlab.hoseo.edu>
2018-03-12 21:44:39 +0900

    카메라 실험

---
+++ Assets/CustomScripts/DisableTracking.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class DisableTracking : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        InputTracking.disablePositionalTracking = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            transform.position = -InputTracking.GetLocalPosition(XRNode.CenterEye);
+            transform.rotation = Quaternion.Inverse(InputTracking.GetLocalRotation(XRNode.CenterEye));
+    }
+}
